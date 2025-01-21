864b70b96e005ac87109e087ac81f83320583490
zhouzhu <68577793+letconstvar@users.noreply.github.com>
2025-01-21 09:59:07 +0800

    test(AntFormPlus): 添加 AntFormPlus 组件的单元测试
    - 创建 AntFormPlus 组件的测试文件
    - 编写测试用例以验证组件的基本功能
    - 包括渲染测试、必填项验证和输入值获取等测试内容

---
+++ packages/components/AntFormPlus/__test__/index.test.tsx
@@ -0,0 +1,66 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import AntFormPlus from "@/components/AntFormPlus/index";
+import type { ColumnPlus } from "@/types/index";
+import "@testing-library/jest-dom";
+import { Form } from "antd";
+
+const columns: ColumnPlus[] = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    formItemProps: {
+      rules: [
+        { required: true, message: "${name} is required", type: "string" },
+      ],
+    },
+  },
+];
+
+beforeAll(() => {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+const Wrapper = () => {
+  const [form] = Form.useForm();
+  return <AntFormPlus form={form} columns={columns} />;
+};
+
+describe("AntFormPlus 组件", () => {
+  it("正确渲染 AntFormPlus 组件", () => {
+    const { container } = render(<Wrapper />);
+    expect(container.querySelector("form")).toBeInTheDocument();
+  });
+
+  it("正确渲染 input name 输入表单", () => {
+    render(<Wrapper />);
+    expect(screen.getByLabelText("Name")).toBeInTheDocument();
+  });
+
+  it("正确渲染 input name 输入表单为必填项", () => {
+    render(<Wrapper />);
+    const input = screen.getByLabelText("Name");
+    expect(input).toBeRequired();
+  });
+
+  it("正确获取 input name 输入表单的值", () => {
+    const { container, getByLabelText } = render(<Wrapper />);
+    const input = getByLabelText("Name");
+
+    fireEvent.change(input, { target: { value: "111" } });
+
+    expect(container.querySelector("#name")).toHaveValue("111");
+  });
+});
