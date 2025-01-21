b29f78362744fdfe91a57531c970b4b801e0d80d
zhouzhu <68577793+letconstvar@users.noreply.github.com>
2025-01-21 09:58:53 +0800

    test: 添加 Jest 配置文件
    - 新增 jest.config.js 文件
    - 配置 TypeScript 和 JsDOM 环境
    - 设置模块名称映射
    - 配置文件扩展名识别

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/types/(.*)$': '<rootDir>/packages/types/$1',
+    '^@/utils/(.*)$': '<rootDir>/packages/utils/src/$1',
+    '^@/components/(.*)$': '<rootDir>/packages/components/$1'
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
